f1707a5d8522785299fe18bc88f3c094ef13ef38
lewiscai2003 <lcai0015@student.monash.edu>
2024-08-28 18:31:10 +1000

    update

--- Week6/app.js
+++ Week6/app.js
@@ -1,44 +1,59 @@
 const mongodb = require("mongodb");
+let ejs = require("ejs");
 const express = require("express");
 const path = require("path");
 
 const MongoClient = mongodb.MongoClient;
-const URL = "mongodb://127.0.0.1:27017";
+const URL = "mongodb://localhost:27017/";
 const client = new MongoClient(URL);
 
 const app = express();
 app.listen(8080);
 app.use(express.urlencoded({ extended: false }));
 
+// Configure Express for EJS
+app.engine("html", ejs.renderFile);
+app.set("view engine", "html");
+
 let db;
 let collection;
 
 async function connectDB() {
-	await client.connect();
-	db = client.db("fit2095");
-	collection = db.collection("week6");
-
-	console.log("Connect to DB successfully");
+    await client.connect();
+    db = client.db("fit2095");
+    collection = db.collection("sales");
+    console.log("Connect to DB successfully");
 }
 
 app.get("/", function (req, res) {
-	res.sendFile(path.join(__dirname, "views", "index.html"));
+    res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.get("/33120102/sales", function (req, res) {
     db.collection("sales")
-    .find({})
-    .toArray(function (err, data) {
-      res.render("listsales", { sales: data });
-    });
+        .find({})
+        .toArray(function (err, data) {
+            //if data is not found, redirect to invalid page
+            if (data.length === 0) {
+                res.sendFile(path.join(__dirname, "views", "noSales.html"));
+            }else{
+                res.render("listsales", { sales: data });
+            }   
+        }
+    );
 });
 
 app.get("/33120102/sales/unpaid", function (req, res) {
     db.collection("sales")
-    .find({status: "unpaid"})
-    .toArray(function (err, data) {
-      res.render("listsales", { sales: data });
-    });
+        .find({ status: "unpaid" })
+        .toArray(function (err, data) {
+            //if data is not found, redirect to invalid page
+            if (data.length === 0) {
+                res.sendFile(path.join(__dirname, "views", "noPaid.html"));
+                return;
+            }
+            res.render("listsales", { sales: data });
+        });
 });
 
 app.get("/33120102/sales/add", function (req, res) {
@@ -58,8 +73,8 @@ app.post("/33120102/sales/add", function (req, res) {
 });
 
 app.get("/33120102/sales/remove", function (req, res) {
-    //delete entire sales document
-    db.collection("sales").deleteMany({});
+    let id = new mongodb.ObjectID(req.query.id);
+    db.collection("sales").deleteOne({ _id: id });
     res.redirect("/33120102/sales");
 });
 
